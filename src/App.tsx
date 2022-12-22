31e019e124be9e21f99a1751376c99b1ecb1b55c
Giorgi-Jalali <jalala18@mail.ru>
2022-12-22 13:48:48 +0400

    change to browserrouter and private to false

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import {
-  createHashRouter,
+  createBrowserRouter,
   createRoutesFromElements,
   RouterProvider,
   Route,
@@ -25,7 +25,7 @@ import PetraLocation from "./locationPages/PetraLocation";
 import TajMahal from "./pages/TajMahal";
 import TajMahalLocation from "./locationPages/TajMahalLocation";
 
-const router = createHashRouter(
+const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />}>
       <Route index element={<HomePage />} />
